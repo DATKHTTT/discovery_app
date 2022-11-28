2a54ed44448cf696efc41e32baba76f7873e372a
Quyet Invisible <turtle.ntq@gmail.com>
2022-11-28 11:15:38 +0700

    test

--- lib/misc/app_icons.dart
+++ lib/misc/app_icons.dart
@@ -2,4 +2,5 @@ class AppIcons {
   static final String baseDir = 'assets/icons/';
   static final String leftBackIcon = baseDir + 'left_back_icon.png';
   static final String hide_icon = baseDir + 'hide_icon.png';
+  
 }
